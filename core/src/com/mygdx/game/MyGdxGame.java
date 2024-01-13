ab8606bcd719c8105a383925d871c19ebfdff554
Tony Yu <itsfishpain@gmail.com>
2024-01-13 14:13:07 +0800

    feat(lifecycle): created lifecycle management

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,27 +5,46 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+/**
+ * This is the simulation lifecycle management class.
+ * {@link https://libgdx.com/wiki/app/the-life-cycle}
+ * 
+ * @author Tony
+ */
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
 		batch.dispose();
 		img.dispose();
 	}
+
 }
