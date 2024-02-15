b261d3ed37201ac2669acf2878666d6030f9c521
Tony Yu <itsfishpain@gmail.com>
2024-02-15 22:56:02 +0800

    feat: static file to load game specific variables

---
+++ core/src/com/mygdx/game/GameConfig.java
@@ -0,0 +1,33 @@
+package com.mygdx.game;
+
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+
+public class GameConfig {
+    public static final int SCREEN_WIDTH;
+    public static final int SCREEN_HEIGHT;
+    public static final boolean RESIZABLE;
+    public static final int FPS = 60;
+    public static final String TITLE = "My GDX Game";
+
+    // Static initializer block to set screen size based on the operating system
+    static {
+        String osName = System.getProperty("os.name").toLowerCase();
+        System.out.println("Operating System Name: " + osName);
+
+        // Assuming you want to use GraphicsEnvironment for non-Windows systems as an
+        // example
+        if (osName.contains("windows")) {
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            SCREEN_WIDTH = screenSize.width;
+            SCREEN_HEIGHT = screenSize.height;
+            RESIZABLE = false;
+        } else {
+            SCREEN_WIDTH = 800;
+            SCREEN_HEIGHT = 600;
+            RESIZABLE = true;
+        }
+    }
+}
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -3,20 +3,19 @@ package com.mygdx.game;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main(String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("My GDX Game");
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
-		// Get the screen size
-		java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-		int screenWidth = (int) screenSize.getWidth() / 4 * 3;
-		int screenHeight = (int) screenSize.getHeight() / 4 * 3;
+        // Set fixed window size
+        config.setWindowedMode(GameConfig.SCREEN_WIDTH, GameConfig.SCREEN_HEIGHT);
+        config.setResizable(GameConfig.RESIZABLE); // Make window not resizable
 
-		config.setWindowedMode(screenWidth, screenHeight);
-		config.setResizable(false);
-		new Lwjgl3Application(new Game(), config);
-	}
+        // Configure other settings
+        config.setForegroundFPS(GameConfig.FPS);
+        config.setTitle(GameConfig.TITLE);
+
+        // Note: For macOS, ensure to start the application with -XstartOnFirstThread JVM argument
+        new Lwjgl3Application(new Game(), config);
+    }
 }
