8513b959841b27b060eb6fd72eca83a2fd802df1
Tony Yu <itsfishpain@gmail.com>
2024-03-07 22:58:02 +0800

    fix: button posisioning
    resized buttons are not registering clicks

--- core/src/com/mygdx/game/GameConfig.java
+++ core/src/com/mygdx/game/GameConfig.java
@@ -1,6 +1,5 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.Gdx;
 import com.mygdx.engine.entity.EntityType;
 import com.mygdx.engine.scene.SceneType;
 import com.mygdx.engine.io.SoundEffects;
